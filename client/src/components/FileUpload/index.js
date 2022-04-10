fb53cf9d64b46f4fe31c32b479e198a4e5e9f162
avpizarro <avpizarro@live.com>
2022-04-10 10:02:40 +1000

    Show all uploaded images

--- client/src/components/FileUpload/index.js
+++ client/src/components/FileUpload/index.js
@@ -5,23 +5,27 @@ import API from "../../utils/API";
 import DeletePhoto from "../IconDeletePhoto";
 import SearchImage from "../IconSearchImage";
 import IconImageUpload from "../IconImageUpload";
+import Modal from "../Modal";
 import "./style.css";
 
 const FileUpload = () =>
 {
+  // Set states for component
   const [file, setFile] = useState("");
   const [fileName, setFileName] = useState("No file chosen");
   const [uploadedFile, setUploadedFile] = useState("");
   const [uploadedFileId, setUploadedFileId] = useState("");
   const [childrenHelp, setChildrenHelp] = useState("");
   const [showUploadForm, setShowUploadForm] = useState(false);
+  const [filePaths, setFilesPaths] = useState("");
+  const [showModal, setShowModal] = useState(false);
 
+  // Function to retrieve the last image uploaded
   async function loadImage()
   {
-    API.getHomeImages()
+    await API.getHomeImages()
       .then((res) =>
       {
-        console.log(res);
         const image = res.data.map((item) =>
         {
           return {
@@ -29,18 +33,20 @@ const FileUpload = () =>
             filePath: item.filePath,
             id: item._id,
           };
-        });
+        })
         setUploadedFile(image[image.length - 1].filePath);
         setUploadedFileId(image[image.length - 1].id);
       })
       .catch((err) => console.log(err));
   }
 
+  // Render the component again when last image has been retrieved
   useEffect(() =>
   {
     loadImage();
   }, []);
 
+  // Function to display the selected file name
   const onChange = (e) =>
   {
     setChildrenHelp("");
@@ -48,44 +54,75 @@ const FileUpload = () =>
     setFileName(e.target.files[0].name);
   };
 
+  // Function to submit and display the file that was selected
   const onSubmit = async (e) =>
   {
-    if (fileName !== "No file chosen") {
-    setFileName("No file chosen");
-    setFile("");
-    setShowUploadForm(false);
-    e.preventDefault();
-    const formData = new FormData();
-    formData.append("file", file);
-    formData.append("upload_preset", "e0q1bp0i");
-    Axios.post("https://api.cloudinary.com/v1_1/dmrpspydu/image/upload",
-      formData
-    ).then(response =>
+    if (fileName !== "No file chosen")
     {
-      console.log("response: ", response.data.public_id);
-      setUploadedFile(`https://res.cloudinary.com/dmrpspydu/image/upload/v1649157923/${response.data.public_id}`);
-      const ImageToAdd = {
-        fileName: file.name,
-        filePath: `https://res.cloudinary.com/dmrpspydu/image/upload/v1649157923/${response.data.public_id}`,
-      };
-      API.saveHomeImg(ImageToAdd);
-    });
-  } else {
-    setChildrenHelp("Choose a file");
-  }
+      setFileName("No file chosen");
+      setFile("");
+      setShowUploadForm(false);
+      e.preventDefault();
+      const formData = new FormData();
+      formData.append("file", file);
+      formData.append("upload_preset", "e0q1bp0i");
+      Axios.post("https://api.cloudinary.com/v1_1/dmrpspydu/image/upload",
+        formData
+      ).then(response =>
+      {
+        setUploadedFile(`https://res.cloudinary.com/dmrpspydu/image/upload/v1649157923/${response.data.public_id}`);
+        const ImageToAdd = {
+          fileName: file.name,
+          filePath: `https://res.cloudinary.com/dmrpspydu/image/upload/v1649157923/${response.data.public_id}`,
+        };
+        API.saveHomeImg(ImageToAdd);
+      });
+    } else
+    {
+      setChildrenHelp("Choose a file");
+    }
   }
 
+  // Function to remove the file from the DB
+  // and to display the previous image uploaded
   const removeImage = async (e) =>
   {
-    console.log("Remove image has been clicked");
-    console.log("file Mongo Id: ", uploadedFileId);
     await API.deleteHomeImg(uploadedFileId);
     await loadImage();
   }
 
-  const uploadImage = (e) =>
+  // Function to display the Upload Form
+  const uploadImage = async (e) =>
   {
     setShowUploadForm(prevShowUploadForm => !prevShowUploadForm);
+  }
+
+  // Function to close the Modal
+  const closeModal = () =>
+  {
+    setShowModal(prevShowModal => !prevShowModal);
+  }
+
+  //  Function to retrieve all the images
+  const loadImages = async (e) =>
+  {
+    console.log("filePaths before", filePaths);
+    setShowModal(prevShowModal => !prevShowModal);
+    await API.getHomeImages()
+      .then((res) =>
+      {
+        const images = res.data.map((item) =>
+        {
+          return {
+            fileName: item.fileName,
+            filePath: item.filePath,
+            id: item._id,
+          };
+        });
+        setFilesPaths(images);
+        console.log(filePaths);
+      })
+      .catch((err) => console.log(err));
   }
 
   // Function to show the upload image form
@@ -124,9 +161,6 @@ const FileUpload = () =>
     )
   }
 
-
-  console.log("showUploadForm", showUploadForm);
-
   return (
     <Fragment>
       {uploadedFile ? (
@@ -139,20 +173,38 @@ const FileUpload = () =>
       ) : null}
       <div>
         <button
+          className="imagesBtn"
           onClick={uploadImage}
-          style={{ backgroundColor: "white", border: "none" }}>
+        >
           <IconImageUpload />
         </button>
         <button
+          className="imagesBtn"
           onClick={removeImage}
-          style={{ backgroundColor: "white", border: "none" }}>
+        >
           <DeletePhoto id={uploadedFileId} />
         </button>
-        <SearchImage />
+        <button
+          className="imagesBtn"
+          onClick={loadImages}
+        >
+          <SearchImage />
+        </button>
       </div>
       {showUploadForm === true ? (
         <ShowUploadImage />
       ) : null}
+      {filePaths ?
+        <Modal
+          show={showModal}
+          close={closeModal}
+          children=
+          {filePaths.map(item => {return <img src={item.filePath} alt={item.fileName} key={item.id} style={{
+          margin:"10px", height: "80px"}}/>})
+          }
+        />
+        : null
+      }
     </Fragment>
   )
 };
